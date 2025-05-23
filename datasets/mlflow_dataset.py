1bba6ef7b360d6f05ffd445b21b5c0e96c683ec7
Jekabs Gritans <jekabs@gritans.lv>
2025-05-23 14:54:17 +0200

    pandas dataset

--- datasets/mlflow_dataset.py
+++ datasets/mlflow_dataset.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 import os
+from re import A
 from typing import Callable
 
 import numpy as np
+import pandas as pd
 import torch
+from matplotlib.pylab import float_
 from omegaconf import DictConfig
 from torch.utils.data import Dataset
 
@@ -13,17 +16,52 @@ from utils.mlflow_utils import download_artifact
 # our transforms are just user defined functions
 Transform = Callable[[torch.Tensor], torch.Tensor]
 
+"""
+df
+
+Required:
+    spectrum: float array (already interpolated not XY). 
+    wavenumber_interval: (min, max) in cm^-1
+
+Functional groups by name (just give in example, not enforced)
+    fg_name: bool
+
+additional targets:
+    target: bool or float (int num donors as just float)
+
+if doing augmentations for synthetic samples, 
+    augmented: bool # to not use for validation/test
+
+dataset split performed at object init, with a local seed so that the same for test/valid.
+"""
+
+#TODO: save head as txt to mlflow. and description of relevant columns. prolly no params
+
 class MLFlowDataset(Dataset):
     """
     Downloads and stores dataset from MLFlow.
     """
 
-    inputs: torch.Tensor # (num_samples, input_features)
-    target: torch.Tensor# (num_samples, output_features)
+    df: pd.DataFrame
 
-    transform: Transform | None # applied to inputs
-    target_names: list[str]
-    pos_weights: torch.Tensor # (num_targets,)
+    spectra: torch.Tensor # (num_samples,) input is always just spectra. not normalized.
+
+    fg_names: list[str] # names of functional groups
+    fg_targets: torch.Tensor # (num_samples, fg) bool
+    fg_pos_weights: torch.Tensor # (fg,) float
+
+    aux_bool_names: list[str] # names of auxiliary bool targets
+    aux_bool_targets: torch.Tensor # (num_samples, aux_targets) bool
+    aux_pos_weights: torch.Tensor # (aux_targets,) float
+
+    aux_float_names: list[str] # names of float targets
+    aux_float_targets: torch.Tensor # (num_samples, float_targets) float
+
+    spectrum_transform: Transform | None # random train transforms only applied to spectra
+
+    cfg: DictConfig
+    dataset_id: str
+    split: str
 
     def __init__(self, cfg: DictConfig, dataset_id: str, split: str, transform: Transform | None = None):
         """
@@ -39,43 +77,57 @@ class MLFlowDataset(Dataset):
         self.dataset_id = dataset_id
         self.transform = transform
         self.split = split
-        self.download()
+        
+    def load_df(self, df: pd.DataFrame):
+        assert "spectrum" in df.columns, "spectrum column not found in dataframe"
+
+        self.df = df
+
+        # spectra
+        self.spectra = torch.Tensor(df["spectrum"].tolist(), dtype=torch.float32, device=self.cfg.device) # type: ignore
+
+        # functional groups
+        cols = []
+        for target in self.fg_names:
+            assert target in df.columns, f"{target} column not found in dataframe"
+            assert df[target].dtype == bool, f"{target} column is not bool"
+            col = torch.tensor(df[target].tolist(), dtype=torch.bool, device=self.cfg.device) # type: ignore
+            cols.append(col)
+
+        self.fg_targets = torch.stack(cols, dim=1)
+
+        fg_pos_counts = self.fg_targets.sum(dim=0).tolist()
+        fg_pos_weights = self._compute_pos_weights(fg_pos_counts)
+        self.fg_pos_weights = torch.tensor(fg_pos_weights, dtype=torch.float32, device=self.cfg.device) # type: ignore
+
+        # auxiliary bool
+        cols = []
+        for target in self.aux_bool_names:
+            assert target in df.columns, f"{target} column not found in dataframe"
+            assert df[target].dtype == bool, f"{target} column is not bool"
+            col = torch.tensor(df[target].tolist(), dtype=torch.bool, device=self.cfg.device) # type: ignore
+            cols.append(col)
+
+        self.aux_bool_targets = torch.stack(cols, dim=1)
+
+        aux_bool_pos_counts = self.aux_bool_targets.sum(dim=0).tolist()
+        aux_bool_pos_weights = self._compute_pos_weights(aux_bool_pos_counts)
+        self.aux_pos_weights = torch.tensor(aux_bool_pos_weights, dtype=torch.float32, device=self.cfg.device) # type: ignore
+
+        # auxiliary float
+        cols = []
+        for target in self.aux_float_names:
+            assert target in df.columns, f"{target} column not found in dataframe"
+            assert df[target].dtype == float, f"{target} column is not float"
+            col = torch.tensor(df[target].tolist(), dtype=torch.float32, device=self.cfg.device)
+            cols.append(col)
+
+        self.aux_float_targets = torch.stack(cols, dim=1)
 
     def download(self):
-        artifacts = [f"{self.split}_inputs.npy", f"{self.split}_target.npy", "target_names.txt", f"{self.split}_pos_counts.txt"]
-
-        # download only the artifacts we need
-        for artifact in artifacts:
-            download_artifact(self.cfg, self.dataset_id, artifact)
-
-        local_dir = os.path.join(self.cfg.runs_path, self.dataset_id)
-
-        inputs_path = os.path.join(local_dir, f"{self.split}_inputs.npy")
-        target_path = os.path.join(local_dir, f"{self.split}_target.npy")
-        target_names_path = os.path.join(local_dir, "target_names.txt")
-        pos_counts_path = os.path.join(local_dir, f"{self.split}_pos_counts.txt")
-
-        # verify all exist
-        for path in [inputs_path, target_path, target_names_path, pos_counts_path]:
-            if not os.path.exists(path):
-                raise FileNotFoundError(f"Artifact {path} does not exist")
-        
-        self.inputs = torch.from_numpy(np.load(inputs_path)).float()
-        self.target = torch.from_numpy(np.load(target_path)).float()
-
-        self.target_names = []
-        with open(target_names_path, "r") as f:
-            for line in f:
-                self.target_names.append(line.strip())
-
-        pos_counts = []
-        with open(pos_counts_path, "r") as f:
-            for line in f:
-                pos_counts.append(int(line.strip()))
-        
-        pos_weights = self._compute_pos_weights(pos_counts)
-        self.pos_weights = torch.tensor(pos_weights, dtype=torch.float32)
-
+        df_path = download_artifact(self.cfg, self.dataset_id, f"{self.split}_df.pkl")
+        self.df = pd.read_pickle(df_path)
+        self.load_df(self.df)
 
     def _compute_pos_weights(self, pos_counts: list[int]) -> list[float]:
         """
@@ -90,26 +142,24 @@ class MLFlowDataset(Dataset):
         Returns:
             list[float]: Positive weights for each target class
         """
-        total_samples = self.inputs.shape[0]
+        total_samples = self.spectra.shape[0]
         pos_weights = []
         
-        for pos_count, target_name in zip(pos_counts, self.target_names):
+        for pos_count in pos_counts:
             # Avoid division by zero
-            assert pos_count > 0, f"No positive samples for target {target_name} in the dataset. it probably shouldn't be predicted."
+            assert pos_count > 0, "Positive count cannot be zero"
             weight = (total_samples - pos_count) / pos_count
             pos_weights.append(weight)
         
         return pos_weights
     
-
     def to(self, device: torch.device) -> MLFlowDataset:
         """
         Move the dataset to the specified device.
         :param device: Device to move the dataset to
         """
-        self.inputs = self.inputs.to(device)
-        self.target = self.target.to(device)
-        self.pos_weights = self.pos_weights.to(device)
+        self.sprctra = self.spectra.to(device)
+        self.targets = {name: target.to(device) for name, target in self.targets.items()}       
 
         return self
 
@@ -118,7 +168,7 @@ class MLFlowDataset(Dataset):
         Get the number of samples in the dataset.
         :return: Number of samples
         """
-        return self.inputs.shape[0]
+        return self.spectra.shape[0]
     
     def __getitem__(self, index) -> dict:
         """
@@ -127,13 +177,16 @@ class MLFlowDataset(Dataset):
         :return: output dictionary
         """
 
-        inputs = self.inputs[index]
-        if self.transform is not None:
-            inputs = self.transform(inputs)
+        out = {}
 
-        target = self.target[index]
+        spectrum = self.spectra[index]
+        if self.spectrum_transform is not None:
+            spectrum = self.spectrum_transform(spectrum)
 
-        out = {"inputs": inputs, "target": target}
+        out["spectrum"] = spectrum
+        out["fg_targets"] = self.fg_targets[index]
+        out["aux_bool_targets"] = self.aux_bool_targets[index]
+        out["aux_float_targets"] = self.aux_float_targets[index]
 
         return out
 

323e711ddc8fcc7c8c93f008aa74bf50a63d252e
Jekabs Gritans <jekabs.gritans@giraffe360.com>
2025-06-11 19:13:43 +0200

    req

--- vastai_provision.sh
+++ vastai_provision.sh
@@ -11,7 +11,7 @@ echo "Cloning repo..." | tee -a /workspace/provision.log
 git clone https://github.com/jekabsGritans/mt_fcgformer.git /workspace/repo 2>&1 | tee -a /workspace/provision.log
 
 if [ -f requirements.txt ]; then
-    pip install --no-cache-dir -r requirements.txt
+    pip install --no-cache-dir -r repo/requirements.txt
 fi
 
 echo "Provisioning complete." | tee -a /workspace/provision.log
